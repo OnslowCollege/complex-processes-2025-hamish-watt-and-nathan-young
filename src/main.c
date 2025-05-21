15e0c2aeac76549b235c238332eebf82aa270fcb
Nathan Young <snawke@DESKTOP-S73ROFJ.>
2025-05-21 18:59:06 +1200

    Create window

--- src/main.c
+++ src/main.c
@@ -1,7 +1,56 @@
-#include <stdio.h>
+#ifdef _WIN32
+#include <windows.h>
 
-int main()
+LRESULT CALLBACK windowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-    printf("Hello, World!");
-    return 0;
+    const char *CLASS_NAME = "Main Window";
+    WNDCLASS wndClass = {};
+
+    wndClass.lpfnWndProc = windowProc;
+    wndClass.hInstance = hInstance;
+    wndClass.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wndClass);
+
+    HWND hwnd = CreateWindowEx(0, CLASS_NAME, "dwmulate", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+                               CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nShowCmd);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 }
+
+LRESULT CALLBACK windowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(hwnd, &ps);
+    }
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+#endif
