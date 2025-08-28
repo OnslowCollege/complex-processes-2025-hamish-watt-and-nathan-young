68764bf42830a3ab111917ee75707edf96b6b735
Nathan Young <nathantayloryoung@gmail.com>
2025-08-28 13:03:16 +1200

    gradient working

--- src/asm/fillgradient.asm
+++ src/asm/fillgradient.asm
@@ -10,27 +10,29 @@ _fillgradient:
     push edi
     push ebx
 
-    sub esp, 0x18
+    sub esp, 52
 
-    mov edi, [ebp+0x8] ; location in memory of the beginning of the colour array
-    mov eax, [ebp+0xc] ; starting colour
-    mov ebx, [ebp+0x10] ; ending colour
-    mov ecx, [ebp+0x18] ; width of colour array in pixels
+    mov edi, [ebp+8] ; location in memory of the beginning of the colour array
+    mov eax, [ebp+12] ; starting colour
+    mov ebx, [ebp+16] ; ending colour
+    mov ecx, [ebp+24] ; width of colour array in pixels
 
+    ; store individual colours out of eax
     mov esi, eax
     and esi, 0xFF
     mov [esp], esi ; blue value
 
     mov esi, eax
-    shr esi, 0x8
+    shr esi, 8
     and esi, 0xFF
-    mov [esp+0x4], esi ; green value
+    mov [esp+4], esi ; green value
 
     mov esi, eax
-    shr esi, 0x10
+    shr esi, 16
     and esi, 0xFF
-    mov [esp+0x8], esi ; red value
+    mov [esp+8], esi ; red value
 
+    ; get per loop increments for colours
     mov esi, ebx,
     and esi, 0xFF
     sub esi, [esp]
@@ -38,64 +40,102 @@ _fillgradient:
     mov eax, esi
     cdq
     idiv ecx,
-    mov [esp+0xc], eax
+    mov [esp+12], eax ; blue increment
+    mov [esp+24], edx ; blue remainder
 
     mov esi, ebx
-    shr esi, 0x8
+    shr esi, 8
     and esi, 0xFF
-    sub esi, [esp+0x4]
+    sub esi, [esp+4]
     xor edx, edx
     mov eax, esi
     cdq
     idiv ecx,
-    mov [esp+0x10], eax
+    mov [esp+16], eax ; green increment
+    mov [esp+28], edx ; green remainder
 
     mov esi, ebx
-    shr esi, 0x10
+    shr esi, 16
     and esi, 0xFF
-    sub esi, [esp+0x8]
+    sub esi, [esp+8]
     xor edx, edx
     mov eax, esi
     cdq
     idiv ecx,
-    mov [esp+0x14], eax
+    mov [esp+20], eax ; red increment
+    mov [esp+32], edx ; red remainder
 
-    shl eax, 0x8 ; put into little endian format (00BBGGRR)
+    mov eax, [ebp+20]
+    lea edx, [edi + eax]
+    mov [esp+48], edx ; ending address of array
+
+    ; accumulators
+    mov DWORD [esp+36], 0
+    mov DWORD [esp+40], 0
+    mov DWORD [esp+44], 0
+
+    mov eax, [ebp+12] ; starting colour
+
+    shl eax, 8 ; put into little endian format (00BBGGRR)
     bswap eax
 
-    mov edx, [esp+0x14]
-    shl edx, 0x8
-    or edx, [esp+0x10]
-    shl edx, 0x8
-    or edx, [esp+0xc]
-
-    mov eax, [ebp+0xc] ; starting colour
-
-    shl edx, 0x8 ; put increments into little endian format
-    bswap edx
-
-    mov ebx, [ebp+0x18]
-    mov ecx, [ebp+0x14]
+    mov ebx, [ebp+24]
     xor esi, esi
-fillgradient_loop:
+.fillgradient_loop:
     mov [edi], eax
 
     add edi, 4
-    sub ecx, 4
     inc esi
 
-    add eax, edx
+    add al, BYTE [esp+12]
+    add ah, BYTE [esp+16]
+    mov ecx, eax
+    shr ecx, 16
+    add cl, BYTE [esp+20]
+    shl ecx, 16
+    mov cx, ax
+    mov eax, ecx
+
+    ; blue channel carry check
+    mov ecx, [esp+36]
+    add ecx, [esp+24]
+    cmp ecx, [ebp+24]
+    jl .no_blue_carry
+    sub ecx, [ebp+24]
+    add eax, 1
+.no_blue_carry:
+    mov [esp+36], ecx
+
+    ; green channel carry check
+    mov ecx, [esp+40]
+    add ecx, [esp+28]
+    cmp ecx, [ebp+24]
+    jl .no_green_carry
+    sub ecx, [ebp+24]
+    add eax, 1 << 8
+.no_green_carry:
+    mov [esp+40], ecx
+
+    ; red channel carry check
+    mov ecx, [esp+44]
+    add ecx, [esp+32]
+    cmp ecx, [ebp+24]
+    jl .no_red_carry
+    sub ecx, [ebp+24]
+    add eax, 1 << 16
+.no_red_carry:
+    mov [esp+44], ecx
 
     cmp esi, ebx
-    jl continue_loop
+    jl .continue_loop
 
-    mov eax, [ebp+0xc]
+    mov eax, [ebp+12]
     xor esi, esi
-continue_loop:
-    test ecx, ecx
-    jnz fillgradient_loop
-
-    add esp, 0x18
+.continue_loop:
+    cmp edi, [esp+48]
+    jb .fillgradient_loop
+.finish:
+    add esp, 52 
 
     pop ebx
     pop edi
